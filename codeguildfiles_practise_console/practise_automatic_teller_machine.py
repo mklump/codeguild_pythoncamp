88381c06a386d7580337b40016a6b50e09308d47
mklump <mklump@gmail.com>
2016-04-06 22:27:25 -0700

    fixed doc string bullet characters not recognized.

--- codeguildfiles_practise_console/practise_automatic_teller_machine.py
+++ codeguildfiles_practise_console/practise_automatic_teller_machine.py
@@ -12,21 +12,21 @@ Write a program that functions as a simple ATM for a single account.
 An account will be a class: it will have a field for the balance.
 
 Write functions for the account class that:
-?Deposit to the account.
-?Check if enough funds for a withdrawal.
-?Withdraw an allowed amount.
-?Calculate 0.5% interest on the account.
+Deposit to the account.
+Check if enough funds for a withdrawal.
+Withdraw an allowed amount.
+Calculate 0.5% interest on the account.
 
 Implement a user interface that lets a user pick each of those actions and updates the account. After each action it will print the balance.
 
 Advanced
-?Save the account balance to a file after each operation. Read that balance on startup so the balance persists across program starts.
-?Add to each account class an account ID number.
-?Allow the user to open more than one account. Let them perform all of the above operations by account number.
+    Save the account balance to a file after each operation. Read that balance on startup so the balance persists across program starts.
+    Add to each account class an account ID number.
+    Allow the user to open more than one account. Let them perform all of the above operations by account number.
 
 Added Friday 10:52AM, and being summarily ignored because this was not part of the original design:
-•Add a function called  get_standing  have it return a bool with whether the account has less than $1000 in it.
-•Predatorily charge a transaction fee every time a withdrawal or deposit happens if the account is in bad standing.
+    Add a function called  get_standing  have it return a bool with whether the account has less than $1000 in it.
+    Predatorily charge a transaction fee every time a withdrawal or deposit happens if the account is in bad standing.
 """
 
 class ATMBankAccount:
