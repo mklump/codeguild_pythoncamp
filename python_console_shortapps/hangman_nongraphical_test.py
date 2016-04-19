8c82bd8b898e6823df3dba44fa31ce427d3b021d
mklump <mklump@gmail.com>
2016-04-19 07:21:20 -0700

    IDE change to pycharm for class, VS 2015 files still remain - we still need them for final project.

--- python_console_shortapps/hangman_nongraphical_test.py
+++ python_console_shortapps/hangman_nongraphical_test.py
@@ -1,8 +1,8 @@
 import unittest
 
 class Test_Hangman_Nongraphical(unittest.TestCase):
-    #def test_A(self):
-    #    self.fail("Not implemented")
+    def test_hangman_nongraphical_main(self):
+        self.skipTest('hangman nongraphical is not yet working.')
 
 if __name__ == '__main__':
     unittest.main()
