369c5594e447e901ecc5cb08db192c5a9ef4b70c
Matthew James K <mklump@gmail.com>
2016-04-25 12:24:44 -0700

    Recommit directory rebuild

---
+++ codeguildfiles_html5CSS3JS/practise_leetspeak.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var convertWordToLeetSpeek = function(originalWord)
+{
+    var originalCharToLeetChar =
+    {
+        "0": "0",
+        "l": "1",
+        "e": "3",
+        "a": "4",
+        "t": "7"
+    };
+};
