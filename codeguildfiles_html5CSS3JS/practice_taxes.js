ef78399dea9bd9486c31a4362c60867c4fba70ba
Matthew James K <mklump@gmail.com>
2016-04-26 11:04:38 -0700

    updated pyramid.css after code review.

--- codeguildfiles_html5CSS3JS/practice_taxes.js
+++ codeguildfiles_html5CSS3JS/practice_taxes.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
     Python Coding Bootcamp (pdxcodeguild)
     Code File for Group practice_taxes.html
     by: Andrew & Matthew James K on 4/25/2016
@@ -28,14 +28,51 @@ use console.log() for output printed statement in this form of JavaScript
 */
 "use strict";
 
-var convertWordToLeetSpeek = function(originalWord)
-{
-    var originalCharToLeetChar =
-    {
-        "0": "0",
-        "l": "1",
-        "e": "3",
-        "a": "4",
-        "t": "7"
-    };
+var grossIncome = 0;
+​
+var taxMap = {
+    "3350": 0.05,
+    "8400": 0.07,
+    "125000": 0.09,
+    "over": 0.099
 };
+​
+var arrayTaxMap = function(taxMap) {
+    var taxBracketArr = [];
+    for (var key in taxMap){
+        taxBracketArr.push(key);
+    };
+    return taxBracketArr;
+};
+​
+var calcTaxes = function(grossIncome, taxBracketArr) {
+​
+    if (grossIncome <= parseInt(taxBracketArr[0])) {
+        return taxObligation = grossIncome * taxMap[taxBracketArr[0]];
+    }
+​
+    if (grossIncome <= parseInt(taxBracketArr[1])) {
+        return taxObligation = 167.5 + ((grossIncome - 3350) * taxMap[taxBracketArr[1]]);
+    }
+​
+    if (grossIncome <= parseInt(taxBracketArr[2])) {
+        return taxObligation = (167.5 + 353.5) + ((grossIncome - 8400) * taxMap[taxBracketArr[2]]);
+    }
+​
+    if (grossIncome > parseInt(taxBracketArr[2])) {
+        return taxObligation = (167.5 + 353.5 + 10494) + ((grossIncome - 125000) * taxMap[taxBracketArr[3]]);
+    }
+};
+
+
+void main()
+{
+
+}
+var taxBracketArr = arrayTaxMap(taxMap);
+var taxObligation = calcTaxes(grossIncome, taxBracketArr);
+​
+console.log(calcTaxes(50000, taxBracketArr));
+console.log(calcTaxes(100000, taxBracketArr));
+console.log(calcTaxes(125000, taxBracketArr));
+console.log(calcTaxes(125001, taxBracketArr));
