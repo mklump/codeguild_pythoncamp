fc12e40c314793baa49bccc427e7f590932a3d92
Matthew James K <mklump@gmail.com>
2016-04-29 13:37:24 -0700

    removed semicolons.

--- codeguildfiles_html5CSS3JS/album.js
+++ codeguildfiles_html5CSS3JS/album.js
@@ -7,7 +7,7 @@
 ​
 function getURLString(){
     return $("#urlAddress").val();
-};
+}
 ​
 function createDelLink(sectionElement){
     var deleteElement = $("<a></a>").text("Delete").attr("href", "");
@@ -16,7 +16,7 @@ function createDelLink(sectionElement){
         sectionElement.remove();
     });
     return deleteElement;
-};
+}
 ​
 function createImageSection(URLString){
     // var albumChild = document.createElement("section");
@@ -27,13 +27,13 @@ function createImageSection(URLString){
     var deleteElement = createDelLink(albumChild);
     albumChild.append(deleteElement);
     return albumChild;
-};
+}
 ​
 function addAlbumSectionBlock(){
     var URLString = getURLString();
     var sectionElement = createImageSection(URLString);
     $("#albumlist").append(sectionElement); // Adding album section child to parent section
-};
+}
 ​
 function addPictureControlSet() {
     $("form").on("submit", function (event) {
@@ -41,15 +41,15 @@ function addPictureControlSet() {
         addAlbumSectionBlock();
         countImages();
     });
-};
+}
 
 function countImages(){
     var parent = $("#albumlist");
     var childCount = parent.children().length;
     $("#imagecount").text("Count of Images:  " + childCount);
-};
+}
 
 $(document).ready(function(){
     //this could be the equivalent of jquery main
     addPictureControlSet();
-});
+})
