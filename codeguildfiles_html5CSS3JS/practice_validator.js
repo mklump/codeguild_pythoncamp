20d51acbc6d41feffb85f1898af42feb972e2f77
Matthew James K <mklump@gmail.com>
2016-05-04 15:25:05 -0700

    finished debug practice_validator input.

--- codeguildfiles_html5CSS3JS/practice_validator.js
+++ codeguildfiles_html5CSS3JS/practice_validator.js
@@ -6,82 +6,112 @@
 "use strict";
 
 /*
- * This helper fuction validates the txtFullName input box using "First Last" format exactly.
- * This is an output creation function, not data transform.
- * @returns {Boolen} true if the validation for this input succeeded, otherwise false.
+ * This helper fuction registers the txtFullName handler using "First Last" format exactly.
  */
 function validateTxtFullName() {
-    var valid = false;
-    $("#txtFullName").on("input", function (event) {
-        event.preventDefault();
-        var txt = this.value;
-        var matchArray = txt.match("([a-zA-Z].+)[ ]([a-zA-Z].+)");
-        if (null === matchArray || "" === matchArray[0] || "" === matchArray[1]) {
-            $("#txtFullName").attr("class", "txtWarning");
-            $("#formStatus").text("Full name is not valid.");
-            valid = true;
-        } else {
-            $("#txtFullName").attr("class", "statusOkay");
-            $("#formStatus").text("");
-            valid = false;
-        }
-    });
-    return valid;
+    $("#txtFullName").on("input", null, this, validateFullName);
 }
 
 /*
- * This helper funtion validates the txtAge input box using YYYY-MM-DD number format exactly.
- * This is an output creation function, not data transform.
- * @returns {Boolen} true if the validation for this input succeeded, otherwise false.
+ * This helper funtion registers the txtAge handler using YYYY-MM-DD number format exactly.
  */
 function validateTxtAge() {
-    var valid = false;
-    $("#txtAge").on("input", function (event) {
-        event.preventDefault();
-        var txt = this.value;
-        var matchArray = txt.match("([0-9]{4})[-]([0-9]{2})[-]([0-9]{2})");
-        if (null === matchArray || "" === matchArray[0] || "" === matchArray[1]) {
-            $("#txtAge").attr("class", "txtWarning");
-            $("#formStatus").text("Age is not valid.");
-            valid = true;
-        } else {
-            $("#txtAge").attr("class", "statusOkay");
-            $("#formStatus").text("");
-            valid = false;
-        }
-    });
-    return valid;
+    $("#txtAge").on("input", null, this, validateAge);
 }
 
 /*
  * This helper funtion validates the txtPhone input box using 555-555-5555 number format exactly.
- * This is an output creation function, not data transform.
- * @returns {Boolen} true if the validation for this input succeeded, otherwise false.
  */
 function validateTxtPhone() {
-    var valid = false;
-    $("#txtPhone").on("input", function (event) {
-        event.preventDefault();
-        var txt = this.value;
-        var matchArray = txt.match("([0-9]{3})[-]([0-9]{3})[-]([0-9]{4})");
-        if (null === matchArray || "" === matchArray[0] || "" === matchArray[1]) {
-            $("#txtPhone").attr("class", "txtWarning");
-            $("#formStatus").text("Phone is not valid.");
-            valid = true;
-        } else {
-            $("#txtPhone").attr("class", "statusOkay");
-            $("#formStatus").text("");
-            valid = false;
-        }
-    });
-    return valid;
+    $("#txtPhone").on("input", null, this, validatePhone);
+}
+
+/*
+ * This function is the delegate callback for validateTxtFullName, and establishes if the #txtFullName
+ * control has either valid user input either true or false.
+ * This is an output creation function, not data transform.
+ * @param {Event} reference access to object members
+ * @returns {Boolean} true if the input validation succeeded, otherwise false
+ */
+function validateFullName(event) {
+    var isValid = new Boolean();
+    var txt = null;
+    if (null !== event)
+        txt = event.target.value
+    else
+        txt = $("#txtFullName")[0].value;
+    var matchArray = txt.match("([a-zA-Z].+)[ ]([a-zA-Z].+)");
+    if (null === matchArray || "" === matchArray[0] || "" === matchArray[1]) {
+        $("#txtFullName").attr("class", "txtWarning");
+        $("#formStatus").text("Full name is not valid.");
+        isValid = false;
+    } else {
+        $("#txtFullName").attr("class", "statusOkay");
+        $("#formStatus").text("");
+        isValid = true;
+    }
+    return isValid;
+}
+
+/*
+ * This function is the delegate callback for validateTxtAge, and establishes if the #txtAge
+ * control has either valid user input either true or false.
+ * This is an output creation function, not data transform.
+ * @param {Event} reference access to object members
+ * @returns {Boolean} true if the input validation succeeded, otherwise false
+ */
+function validateAge(event) {
+    var isValid = new Boolean();
+    var txt = null;
+    if (null !== event)
+        txt = event.target.value
+    else
+        txt = $("#txtAge")[0].value;
+    var matchArray = txt.match("([0-9]{4})[-]([0-9]{2})[-]([0-9]{2})");
+    if (null === matchArray || "" === matchArray[0] || "" === matchArray[1]) {
+        $("#txtAge").attr("class", "txtWarning");
+        $("#formStatus").text("Age is not valid.");
+        isValid = false;
+    } else {
+        $("#txtAge").attr("class", "statusOkay");
+        $("#formStatus").text("");
+        isValid = true;
+    }
+    return isValid;
+}
+
+/*
+ * This function is the delegate callback for validateTxtPhone, and establishes if the #txtPhone
+ * control has either valid user input either true or false.
+ * This is an output creation function, not data transform.
+ * @param {Event>} reference access to object members
+ * @returns {Boolean} true if the input validation succeeded, otherwise false
+ */
+function validatePhone(event) {
+    var isValid = new Boolean();
+    var txt = null;
+    if (null !== event)
+        txt = event.target.value
+    else
+        txt = $("#txtPhone")[0].value;
+    var matchArray = txt.match("([0-9]{3})[-]([0-9]{3})[-]([0-9]{4})");
+    if (null === matchArray || "" === matchArray[0] || "" === matchArray[1]) {
+        $("#txtPhone").attr("class", "txtWarning");
+        $("#formStatus").text("Phone is not valid.");
+        isValid = false;
+    } else {
+        $("#txtPhone").attr("class", "statusOkay");
+        $("#formStatus").text("");
+        isValid = true;
+    }
+    return isValid;
 }
 
 /*
  * This helper function accepts the total input validation summary of all inputs, and set the
  * form input status accordingly. This is an output creation function, not data transform.
  */
-function createValidationSummary(isSummaryValid) {
+function getValidationSummary(isSummaryValid) {
     if (true === isSummaryValid)
         $("#formStatus").attr("class", "statusOkay").text("Input submission succeeded.");
     else
@@ -90,36 +120,26 @@ function createValidationSummary(isSummaryValid) {
 
 /*
  * This helper function call each of the input box field validator functions before submission.
- * This is an output creation function, not data transform.
  */
 function validateFormInput() {
-    var summaryValidation = validateTxtFullName() &&
-        validateTxtAge() &&
-        validateTxtPhone();
+    var summaryValidation = validateFullName(null) &&
+        validateAge(null) &&
+        validatePhone(null);
 
-    createValidationSummary(summaryValidation);
-}
-
-/*
- * This helper function sets the input event handlers by calling them directly.
- * This is an output creation function, not data transform.
- */
-function createInputHandlers() {
-    validateTxtFullName();
-    validateTxtAge();
-    validateTxtPhone();
+    getValidationSummary(summaryValidation);
 }
 
 /*
  * This helper function registers all the required event handlers for this page.
- * This is an output creation function, not data transform.
  */
 function registerGlobalEventHandlers() {
     $("form").on("submit", function (event) {
         event.preventDefault();
         validateFormInput();
     });
-    createInputHandlers();
+    validateTxtFullName();
+    validateTxtAge();
+    validateTxtPhone();
 }
 
 /*
